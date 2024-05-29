fd583bfb5192242ac1870a995397e14558aa513c
tharikthajudeen <tharikbinthajudeen65@gmail.com>
2024-05-29 23:19:36 +0530

    Initial commit

---
+++ src/main/java/com/example/hotel_booking_backend/HotelBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotel_booking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hotel_booking_backend/HotelBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotel_booking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
